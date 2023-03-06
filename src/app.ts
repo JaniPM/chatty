057dad57b8be988645fd3a28f46aa5832209511d
JaniPM <jani.siivola@moraius.com>
2023-03-06 15:33:20 +0200

    Use express

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,22 @@
-import Fastify from 'fastify';
-import routes from './routes';
+import express, { Express } from 'express';
+import cors from 'cors';
+import router from './routes';
+import { logRequest } from './middleware/log-request';
+import { logger } from './core/logger';
 
-function init(opts = {}) {
-  const app = Fastify(opts);
-  app.register(routes);
-  return app;
-}
+const app: Express = express();
 
-export default init;
+// Setup middleware
+app.use(cors());
+app.use(logRequest);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Attach routes
+app.use('/api/v1/', router);
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  logger.info(`Server is running at http://localhost:${port}`);
+});
